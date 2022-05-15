ad1cac1995ad28a9b75200a093590135311e6a26
Dany Preisz <danialbertopreisz@gmail.com>
2022-05-15 19:44:14 -0300

    actualizacion 15 de mayo

---
+++ javascript/js/holamundo.js
@@ -0,0 +1,17 @@
+"use strict";
+alert("Hola Mundo desde un archivo externo");
+//Éste es un mensaje de la consola.
+const pi = 3.14;
+var nombre = "Dani";
+console.log("Mi nombre es: ", nombre);
+var x = "3.1416";
+var publicData = "Hola";
+function saludo() {
+  let nombre = "Preisz";
+  console.log(nombre);
+  let edad = 47;
+  console.log(edad);
+}
+console.log("Mi nombre es: ", nombre);
+console.log(edad);
+saludo();
