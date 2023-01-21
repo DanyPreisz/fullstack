6801f78884779dc258c53b32107af44d24351d2c
Dany Preisz <danialbertopreisz@gmail.com>
2023-01-21 16:57:09 -0300

    .

--- python/Two_Sum.py
+++ python/Two_Sum.py
@@ -7,20 +7,20 @@ if __name__ == '__main__':
 	dato = int()
 	tamano = int()
 	target = int()
-	print("Ingrese la suma de dos números.")
+	print("Ingrese la suma de dos numeros.")
 	target = int(input())
-	print("Ingrese la cantidad de números.")
+	print("Ingrese la cantidad de numeros.")
 	tamano = int(input())
-	print("Ingrese los ",tamano," números.")
+	print("Ingrese los ",tamano," numeros.")
 	dimnumeros = [int() for ind0 in range(tamano)]
 	for i in range(tamano):
 		dato = int(input())
 		dimnumeros[i] = dato
 	for i in range(tamano):
-		print("Índice ",i," contiene: ",dimnumeros[i])
+		print("indice ",i," contiene: ",dimnumeros[i])
 	for i in range(tamano+1):
 		for j in range(i+1,tamano):
 			if (dimnumeros[i])+(dimnumeros[j])==(target):
-				print("El par de números es [",dimnumeros[i],",",dimnumeros[j],"]")
-				print("El par de índices es [",i,",",j,"]")
+				print("El par de numeros es [",dimnumeros[i],",",dimnumeros[j],"]")
+				print("El par de indices es [",i,",",j,"]")
 
