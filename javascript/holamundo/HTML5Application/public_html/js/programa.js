4d13f7cf0380f346f682ebcba4c0c52e15b7d8d6
Dany Preisz <danialbertopreisz@gmail.com>
2023-02-23 23:40:01 -0300

    .

---
+++ javascript/holamundo/HTML5Application/public_html/js/programa.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hola consola');
+var x; 
+x = "3.1416";
+var publicData = "Hola";
+document.write("Hola Mundo con JavaScript desde un archivo externo .js");//Usa me método write del objeto document
+
